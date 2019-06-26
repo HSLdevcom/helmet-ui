568ce7ffd2c86c72cc55aa850cc7dfe950eb8d85
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2019-06-26 16:19:10 +0300

    Travis build & deployment (#8)
    Build with Windows, publish to Github.

---
+++ forge-config.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+    packagerConfig: {
+        appBundleId: 'fi.hsl.helmet.ui'
+    },
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                name: "helmet_ui"
+            }
+        },
+        {
+            name: "@electron-forge/maker-deb",
+            config: {}
+        }
+    ],
+    publishers: [
+        {
+            name: '@electron-forge/publisher-github',
+            config: {
+                repository: {
+                    owner: 'HSLdevcom',
+                    name: 'helmet-ui'
+                },
+                draft: true,
+                prerelease: true,
+                authToken: process.env.GITHUB_TOKEN
+            }
+        }
+    ],
+    buildIdentifier: `${new Date().getTime()}`
+}
