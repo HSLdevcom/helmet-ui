4a383480bb738da6094c92d111b106f106a4dbc6
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2019-06-27 15:34:35 +0300

    simple settings dialog

--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,7 @@
 const ps = require('python-shell')
 
 let worker
-const script = `${__dirname}/worker.py`
-const opts = { mode: 'json', pythonOptions: ['-u'] }
-const command = { iterations: 5 };
+const settings = {};
 
 const setStatus = (text) => {
     window.document
@@ -12,56 +10,103 @@ const setStatus = (text) => {
 }
 
 const onMessage = (json) => {
-    console.log(json)
+    console.debug('Message from Python:', JSON.stringify(json))
     if (json.msg) setStatus(json.msg)
 }
 
-const end = (err, code, signal) => {
+const onEnd = (err, code, signal) => {
     if (err) throw err
     worker = null
-    setStatus(`Python exited with code ${code}, signal ${signal}.`)
+    console.debug(`Python exited with code ${code}, signal ${signal}.`)
 }
 
-const iterationsChange = (e) => {
+const settingsChange = (e) => {
+
+    const emme = window.document.getElementById("emme").files[0]
+    const data = window.document.getElementById("data").files[0]
     const i = window.document.getElementById("iterations").value
-    command.iterations = parseInt(i)
-}
+    const python = window.document.getElementById("python").files[0]
+    const scripts = window.document.getElementById("scripts").files[0]
 
-const emmeChange = (e) => {
-    const file = window.document.getElementById("emme").files[0].path
-    command.emme = file;
-}
-
-const dataChange = (e) => {
-    const file = window.document.getElementById("data").files[0].path
-    command.data = file;  
+    settings.emme = emme ? emme.path : settings.emme
+    settings.data = data ? data.path : settings.data
+    settings.iterations = i ? parseInt(i) : settings.iterations
+    settings.python = python ? python.path : settings.python
+    settings.scripts = scripts ? scripts.path : settings.scripts
+    
+    console.debug(settings)
 }
 
 const runStop = (e) => {
-    console.log(command)
+
+    console.log(settings)
+    
     if (worker) {
         worker.terminate(1)
         worker = null
     } else {
-        worker = new ps.PythonShell(script, opts)
+        
+        const opts = {
+            mode: 'json',
+            pythonOptions: ['-u'],
+            pythonPath: settings.python
+        }
+
+        const msg = {
+            emme: settings.emme,
+            data: settings.data,
+            iterations: settings.iterations
+        }
+        console.log(msg);
+
+        worker = new ps.PythonShell(settings.scripts, opts)
         worker.on('message', onMessage)
-        worker.send(command)
-        worker.end(end)
+        worker.send(msg)
+        worker.end(onEnd)
     }
+}
+
+const closeDialog = (e) => {
+    // TODO persist settings
+    document.getElementById("settings").close()
+}
+
+const settingsClick = (e) => {
+    document.getElementById("settings").show()
 }
 
 window.document
     .getElementById("emme")
-    .addEventListener('change', emmeChange)
+    .addEventListener('change', settingsChange)
 
 window.document
     .getElementById("data")
-    .addEventListener('change', dataChange)
+    .addEventListener('change', settingsChange)
 
 window.document
     .getElementById("iterations")
-    .addEventListener('change', iterationsChange);
+    .addEventListener('change', settingsChange)
 
 window.document
-    .querySelector("button")
+    .getElementById("python")
+    .addEventListener('change', settingsChange)
+
+window.document
+    .getElementById("scripts")
+    .addEventListener('change', settingsChange)
+
+window.document
+    .getElementById("settingsButton")
+    .addEventListener('click', settingsClick)
+
+window.document
+    .getElementById("dialogOkButton")
+    .addEventListener('click', closeDialog)
+
+window.document
+    .getElementById("dialogCancelButton")
+    .addEventListener('click', closeDialog)
+
+window.document
+    .getElementById("runStopButton")
     .addEventListener('click', runStop)
