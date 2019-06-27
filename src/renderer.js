f7a80d5cd0380005cb8f4ec1ddcd1c038bba5197
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2019-06-27 16:00:45 +0300

    persist settings

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,29 @@
 const ps = require('python-shell')
+const Store = require('electron-store')
 
 let worker
-const settings = {};
+
+const schema = {
+	'emme.path': {
+		type: 'string',
+    },
+    'data.path': {
+        type: 'string',
+    },
+    'python.path': {
+        type: 'string',
+    },
+    'helmet.remote.path': {
+        type: 'string',
+    },
+	'helmet.run.iterations': {
+        type: 'number',
+        minimum: 1,
+        maximum: 100,
+        default: 5
+	}
+};
+const store = new Store(schema)
 
 const setStatus = (text) => {
     window.document
@@ -24,44 +46,55 @@ const settingsChange = (e) => {
 
     const emme = window.document.getElementById("emme").files[0]
     const data = window.document.getElementById("data").files[0]
-    const i = window.document.getElementById("iterations").value
+    const iterations = window.document.getElementById("iterations").value
     const python = window.document.getElementById("python").files[0]
-    const scripts = window.document.getElementById("scripts").files[0]
+    const remote = window.document.getElementById("scripts").files[0]
 
-    settings.emme = emme ? emme.path : settings.emme
-    settings.data = data ? data.path : settings.data
-    settings.iterations = i ? parseInt(i) : settings.iterations
-    settings.python = python ? python.path : settings.python
-    settings.scripts = scripts ? scripts.path : settings.scripts
-    
-    console.debug(settings)
+    if (emme) {
+        store.set('emme.path', emme.path)
+    }
+    if (data) {
+        store.set('data.path', data.path)
+    }
+    if (python) {
+        store.set('python.path', python.path)
+    }
+    if (remote) {
+        store.set('helmet.remote.path', remote.path)
+    }
+    if (iterations) {
+        store.set('helmet.run.iterations', parseInt(iterations))
+    }    
 }
 
 const runStop = (e) => {
-
-    console.log(settings)
     
     if (worker) {
         worker.terminate(1)
         worker = null
     } else {
         
+        const pythonPath = store.get('python.path')
+        const remoteScript = store.get('helmet.remote.path');
+
+        console.debug(pythonPath, remoteScript)
+
         const opts = {
             mode: 'json',
             pythonOptions: ['-u'],
-            pythonPath: settings.python
+            pythonPath: pythonPath
         }
 
-        const msg = {
-            emme: settings.emme,
-            data: settings.data,
-            iterations: settings.iterations
+        const command = {
+            emme: store.get('emme.path'),
+            data: store.get('data.path'),
+            iterations: store.get('helmet.run.iterations')
         }
-        console.log(msg);
+        console.log(command);
 
-        worker = new ps.PythonShell(settings.scripts, opts)
+        worker = new ps.PythonShell(remoteScript, opts)
         worker.on('message', onMessage)
-        worker.send(msg)
+        worker.send(command)
         worker.end(onEnd)
     }
 }
