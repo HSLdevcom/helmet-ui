36b9af5da88127b386642f49ab053dfed8484a81
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2019-07-03 17:12:39 +0300

    display selected values in UI

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const ps = require('python-shell')
 const Store = require('electron-store')
 const config = require('./config');
@@ -7,7 +8,41 @@ const props = config.store.properties;
 const store = new Store(config.store.schema);
 
 // TODO set others too
-window.document.getElementById('iterations').value = store.get(props.Iterations)
+
+const setLabel = (id, txt, title) => {
+    const label = window.document.getElementById(id)
+    if (label) {
+        label.innerHTML = txt
+        label.setAttribute('title', title);
+    }
+}
+
+const initSettings = () => {
+
+    const emme = store.get(props.EmmePath)
+    if (emme) {
+        setLabel('emme-label', path.basename(store.get(props.EmmePath)), store.get(props.EmmePath));
+    }
+
+    const data = store.get(props.DataPath)
+    if (data) {
+        setLabel('data-label', path.basename(store.get(props.DataPath)), store.get(props.DataPath));
+    }    
+
+    const python = store.get(props.PythonPath)
+    if (python) {
+        setLabel('python-label', path.basename(store.get(props.PythonPath)), store.get(props.PythonPath));
+    }
+
+    const helmet = store.get(props.HelmetPath)
+    if (helmet) {
+        setLabel('scripts-label', path.basename(store.get(props.HelmetPath)), store.get(props.HelmetPath));    
+    }
+
+    window.document.getElementById('iterations').value = store.get(props.Iterations)
+}
+
+initSettings();
 
 const setStatus = (text) => {
     window.document
@@ -43,16 +78,20 @@ const settingsChange = (e) => {
     const scripts = window.document.getElementById('scripts').files[0]
 
     if (emme) {
-        store.set(props.EmmePath, emme.path)
+        store.set(props.EmmePath, emme.path)        
+        setLabel('emme-label', path.basename(emme.path), emme.path);
     }
     if (data) {
         store.set(props.DataPath, data.path)
+        setLabel('data-label', path.basename(data.path), data.path);
     }
     if (python) {
         store.set(props.PythonPath, python.path)
+        setLabel('python-label', path.basename(python.path), python.path);
     }
     if (scripts) {
         store.set(props.HelmetPath, scripts.path)
+        setLabel('scripts-label', path.basename(scripts.path), scripts.path);
     }
     if (iterations) {
         store.set(props.Iterations, parseInt(iterations))
@@ -60,12 +99,12 @@ const settingsChange = (e) => {
 }
 
 const runStop = (e) => {
-    
+
     if (worker) {
         worker.terminate(1)
         worker = null
     } else {
-        
+
         const pythonPath = store.get(props.PythonPath)
         const helmetPath = store.get(props.HelmetPath)
         const helmetScript = `${helmetPath}/helmet_remote.py`
