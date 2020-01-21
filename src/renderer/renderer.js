613ba4789d27bda9c3076d103ae40bec20076ef3
Mew-www <mew.proxy@hotmail.com>
2020-01-21 17:41:39 +0200

    Cleanup renderer.js in preparation for removal

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,122 +1,16 @@
-const path = require('path');
-const {webFrame, ipcRenderer} = require('electron');
 const _ = require('lodash');
 const ps = require('python-shell');
-const Store = require('electron-store');
-const child_process = require('child_process');
-const config = require('../config');
-
-// Disable content zooming (it seems prone to glitching on Windows). https://github.com/electron/electron/issues/15496
-webFrame.setZoomLevel(1);
-webFrame.setVisualZoomLevelLimits(1, 1);
-webFrame.setLayoutZoomLevelLimits(0, 0);
-
-let worker;
-const store = new Store({'cwd': config.store.saveDir});
-
-/**
- * Set status message, for displaying log messages above DEBUG level.
- */
-function setMessage(text, isError) {
-  const message = document.getElementById('message');
-  message.textContent = text;
-  message.setAttribute('class', isError ? 'error' : '');
-}
-
-/**
- * Set simulation state text.
- */
-function setState(status) {
-
-  const STATE_LABELS = {
-    starting: 'Simulaatio aloitettu.',
-    preparing: 'Simulaatiota valmistellaan.',
-    running: 'Simulaatio käynnissä.',
-    failed: 'Simulaatio epäonnistui.',
-    aborted: 'Simulaatio epäonnistui.',
-    finished: 'Simulaatio päättynyt.'
-  };
-
-  const {state, log, results} = status || {};
-
-  document.getElementById('status-state').innerHTML = STATE_LABELS[state] || '';
-}
-
-/**
- * Update the results panel with given results.
- */
-function setResults(status) {
-
-  const {log, results} = status || {};
-  const logLink = document.getElementById('log-link');
-  const keyValues = document.getElementById("key-values");
-
-  if (status && log) {
-    logLink.setAttribute('href', `file:///${log}`);
-    logLink.setAttribute('style', 'visibility:visible;');
-  } else {
-    logLink.setAttribute('style', 'visibility:hidden;');
-  }
-
-  if (results && !_.isEmpty(results)) {
-    const formatted = _.flatMap(results, (res, i) => {
-      const title = `Iteraatio ${i + 1}`;
-      const values = _.map(res, (v, k) => `- ${k} = ${v}`);
-      return _.concat(title, values).join('<br/>');
-    });
-    keyValues.innerHTML = formatted.join('<br/><br/>');
-
-    const resultsDiv = document.getElementById("results");
-    resultsDiv.scrollTop += resultsDiv.offsetTop;
-  } else {
-    keyValues.innerHTML = '';
-  }
-}
-
-/**
- * Set current iteration status.
- */
-function setCurrentIteration(status) {
-
-  const i = status ? status.current : 0;
-  const state = status ? status.state : null;
-
-  const LABELS = {
-    starting: 'Käynnistetään..',
-    preparing: 'Valmistellaan..',
-    running: `Iteraatio ${i} käynnissä..`,
-    failed: `Iteraatio ${i} epäonnistui.`,
-    aborted: 'Simuloinnin alustus epäonnistui.',
-    finished: ' ',
-  };
-
-  const e = window.document.getElementById('status-current');
-  e.innerHTML = LABELS[state] || '';
-}
-
-/**
- * Set progress indicators and update progress bar.
- */
-function setProgress(completed, failed, total) {
-  // About 30min per ajo, tarkoituksena löytää ekvivalenssi, viimeinen ajo saattaa kestää pidempään
-  const txt = window.document.getElementById('status-progress');
-  const bar = document.querySelector("#progressbar .percentage");
-  const progress = Math.min(100, Math.round((completed + failed) / total * 100));
-  const color = failed > 0 ? 'red' : 'navy';
-
-  txt.innerHTML = `${completed} / ${total} iteraatiota suoritettu, ${failed} virhettä.`;
-  bar.setAttribute("style", `width:${progress}%; background-color:${color};`);
-}
 
 /**
  * Reset and hide simulation status panel and results.
  */
 function resetStatus() {
-  setState(null);
-  setCurrentIteration(null);
-  setProgress(0, 0, store.get('iterations'));
-  setMessage("");
-  setResults(null);
+  document.getElementById('status-state').innerHTML = '';
+  document.getElementById('status-current').innerHTML = '';
+  document.getElementById('status-progress').innerHTML = "0 / 0 iteraatiota suoritettu, 0 virhettä.";
+  document.querySelector("#progressbar .percentage").setAttribute("style", "width: 0%; background-color: 'navy'};");
+  document.getElementById('message').textContent = "";
+  document.getElementById('log-link').setAttribute('style', 'visibility:hidden;');
   document.getElementById('status-panel').setAttribute('style', 'visibility:hidden;');
 }
 
@@ -125,14 +19,53 @@ function resetStatus() {
  */
 function handleMsg(json) {
   if (json.level !== 'DEBUG') {
-    setMessage(json.message, json.level === 'ERROR');
+    const message = document.getElementById('message');
+    message.textContent = json.message;
+    message.setAttribute('class', json.level === 'ERROR' ? 'error' : '');
   }
   if (json.status) {
     document.getElementById('status-panel').setAttribute('style', 'visibility:visible;');
-    setState(json.status);
-    setProgress(json.status.completed, json.status.failed, json.status.total);
-    setCurrentIteration(json.status);
-    setResults(json.status);
+    const STATE_LABELS = {
+      starting: 'Simulaatio aloitettu.',
+      preparing: 'Simulaatiota valmistellaan.',
+      running: 'Simulaatio käynnissä.',
+      failed: 'Simulaatio epäonnistui.',
+      aborted: 'Simulaatio epäonnistui.',
+      finished: 'Simulaatio päättynyt.'
+    };
+    document.getElementById('status-state').innerHTML = STATE_LABELS[json.status.state];
+    // Progress: About 30min per ajo, tarkoituksena löytää ekvivalenssi, viimeinen ajo saattaa kestää pidempään
+    document.getElementById('status-progress').innerHTML = (
+      `${json.status.completed} / ${json.status.total} iteraatiota suoritettu, ${json.status.failed} virhettä.`
+    );
+    document.querySelector("#progressbar .percentage")
+      .setAttribute(
+        "style",
+        `width: ${Math.min(100, Math.round((json.status.completed + json.status.failed) / json.status.total * 100))}%; background-color: ${json.status.failed > 0 ? 'red' : 'navy'};`
+      );
+    const LABELS = {
+      starting: 'Käynnistetään..',
+      preparing: 'Valmistellaan..',
+      running: `Iteraatio ${json.status.current} käynnissä..`,
+      failed: `Iteraatio ${json.status.current} epäonnistui.`,
+      aborted: 'Simuloinnin alustus epäonnistui.',
+      finished: ' ',
+    };
+    document.getElementById('status-current').innerHTML = LABELS[json.status.state];
+    const {log, results} = json.status;
+    document.getElementById('log-link').setAttribute('href', `file:///${log}`);
+    document.getElementById('log-link').setAttribute('style', 'visibility:visible;');
+    if (results && !_.isEmpty(results)) {
+      const formatted = _.flatMap(results, (res, i) => {
+        const title = `Iteraatio ${i + 1}`;
+        const values = _.map(res, (v, k) => `- ${k} = ${v}`);
+        return _.concat(title, values).join('<br/>');
+      });
+      document.getElementById("key-values").innerHTML = formatted.join('<br/><br/>');
+      document.getElementById("results").scrollTop += document.getElementById("results").offsetTop;
+    } else {
+      document.getElementById("key-values").innerHTML = '';
+    }
   }
 }
 
@@ -154,7 +87,7 @@ function setControlsEnabled(isEnabled) {
 /**
  * Start or stop helmet-model-system.
  */
-function runStop(e) {
+function runStop(worker, store) {
 
   resetStatus();
 
@@ -191,7 +124,6 @@ function runStop(e) {
       log_level: 'DEBUG', // TODO make configurable
     };
 
-    ipcRenderer.send('console.log', `running:  ${opts.pythonPath}  ${opts.pythonOptions}  ${helmet}\n`.split('  ').join('\n  '));
     worker = new ps.PythonShell(helmet, opts);
     worker.on('message', (json) => {
       console.debug('[message]', json);
@@ -203,9 +135,10 @@ function runStop(e) {
     });
     worker.on('error', (err) => {
       console.error('[error]', err);
-      setMessage(err.message, true);
+      const message = document.getElementById('message');
+      message.textContent = err.message;
+      message.setAttribute('class', 'error');
     });
-    ipcRenderer.send('console.log', `writing the python process stdin: ${JSON.stringify(command, null, " ")}\n`);
     worker.send(command);
     worker.end((err, code, signal) => {
       worker = null;
@@ -223,7 +156,7 @@ function runStop(e) {
 /**
  * Check and notify on missing settings.
  */
-function validateSettings() {
+function validateSettings(store) {
 
   const emp = store.get('emme_project_file_path');
   if (!emp) {
@@ -257,5 +190,3 @@ function validateSettings() {
 
   return true;
 }
-
-resetStatus();
