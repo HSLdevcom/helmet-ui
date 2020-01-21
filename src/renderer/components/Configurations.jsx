42cdadc941da89bce62a9e510e5ad0c7cd6634b3
Mew-www <mew.proxy@hotmail.com>
2020-01-21 14:54:18 +0200

    Enable creation and deletion of scenarios

--- src/renderer/components/Configurations.jsx
+++ src/renderer/components/Configurations.jsx
@@ -21,31 +21,61 @@ class Configurations extends React.Component {
           use_fixed_transit_cost: true,
           iterations: 10
         },
-      ]
+      ],
+      open_scenario_id: null,
     };
   }
 
   render() {
-    return <div>
+    return <div className="ConfigurationsWrapper">
       <RunConfiguration
         scenarios={this.state.scenarios}
-      />
-      <ScenarioConfiguration
-        scenario={this.state.scenarios[0]}
-        updateScenario={(newValues) => {
+        open_scenario_id={this.state.open_scenario_id}
+        setOpenScenarioId={(scenario_id) => {
+          this.setState({open_scenario_id: scenario_id});
+        }}
+        createNewScenario={(name) => {
+          const newId = Math.max.apply(null, this.state.scenarios.map((s) => s.id)) + 1;
+          // Create the new scenario in "scenarios" array first
           this.setState({
-            scenarios: this.state.scenarios.map((s) => {
-              return s.id === newValues.id ? {...s, ...newValues} : s
+            scenarios: this.state.scenarios.concat({
+              id: newId,
+              name: name,
+              emme_project_file_path: undefined,
+              data_folder_path: undefined,
+              use_fixed_transit_cost: false,
+              iterations: 10,
             })
+          });
+          // Then set open_scenario_id (having open_scenario as reference causes sub-elements to be bugged because of different object reference)
+          this.setState({
+            open_scenario_id: newId
           })
         }}
-        deleteScenario={(scenario) => {
-          if (confirm(`Oletko varma skenaarion ${scenario.name} poistosta?`)) {
-            console.log(`Deleting configuration #${scenario.id}`)
-            // No-op for now
-          }
-        }}
       />
+      {this.state.open_scenario_id !== null ?
+        <ScenarioConfiguration
+          scenario={this.state.scenarios.find((s) => s.id === this.state.open_scenario_id)}
+          updateScenario={(newValues) => {
+            this.setState({
+              scenarios: this.state.scenarios.map((s) => {
+                return s.id === newValues.id ? {...s, ...newValues} : s
+              })
+            })
+          }}
+          deleteScenario={(scenario) => {
+            if (confirm(`Oletko varma skenaarion ${scenario.name} poistosta?`)) {
+              this.setState({
+                open_scenario_id: null,
+                scenarios: this.state.scenarios.filter((s) => s.id !== scenario.id)
+              });
+              window.location.reload();  // Vex-js dialog input gets stuck otherwise
+            }
+          }}
+        />
+        :
+        ""
+      }
     </div>;
   }
 }
--- src/renderer/components/RunConfiguration.jsx
+++ src/renderer/components/RunConfiguration.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+// vex-js imported globally in index.html, since we cannot access webpack config in electron-forge
 
 class RunConfiguration extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       scenarios_to_run: [],
-      open_scenario: undefined,
     };
   }
 
@@ -13,6 +13,7 @@ class RunConfiguration extends React.Component {
     return <div className="RunConfiguration">
       <div className="RunConfiguration__heading">Lis&auml;&auml; skenaario(t) ajettavaksi, tai luo uusi skenaario</div>
       <div className="RunConfiguration__saved-scenarios">
+        {/* Create table of all scenarios [<Button-To-Add-As-Runnable> <Button-To-Open-Configuration>] */}
         {this.props.scenarios.map((s) => {
           const isActive = this.state.scenarios_to_run.find((scenario) => scenario.id === s.id);
           return <div className="RunConfiguration__scenario">
@@ -31,14 +32,47 @@ class RunConfiguration extends React.Component {
             </button>
             &nbsp;
             <button className={"RunConfiguration__scenario-name-config-btn" + (
-                      this.state.open_scenario.id === s.id ? " RunConfiguration__scenario-name-config-btn--active" : ""
+                      this.props.open_scenario_id === s.id ? " RunConfiguration__scenario-name-config-btn--active" : ""
                     )}
                     onClick={(e) => {
-                      // todo
+                      this.props.setOpenScenarioId(s.id);
                     }}
             >{s.name}</button>
           </div>;
         })}
+        <div className="RunConfiguration__saved-scenarios-footer">
+          <button className="RunConfiguration__add-new-scenario-btn"
+                  onClick={(e) => {
+                    const promptCreation = (previousError) => {
+                      vex.dialog.prompt({
+                        message: (previousError ? previousError : "") + "Anna uuden skenaarion nimike:",
+                        placeholder: '',
+                        callback: (newScenarioName) => {
+                          let error = "";
+                          // Check for cancel button press
+                          if (newScenarioName === false) {
+                            return;
+                          }
+                          // Check input for initial errors
+                          if (newScenarioName === "") {
+                            error = "Nimike on pakollinen, tallennettavaa tiedostonime\u00e4 varten. ";
+                          }
+                          if (this.props.scenarios.map((s) => s.name).includes(newScenarioName)) {
+                            error = "Nimike on jo olemassa, valitse toinen nimi tai poista olemassa oleva ensin. ";
+                          }
+                          // Handle recursively any input errors (mandated by the async library since prompt isn't natively supported in Electron)
+                          if (error) {
+                            promptCreation(error);
+                          } else {
+                            this.props.createNewScenario(newScenarioName);
+                          }
+                        }
+                      });
+                    };
+                    promptCreation();
+                  }}
+          >Uusi skenaario</button>
+        </div>
       </div>
     </div>;
   }
