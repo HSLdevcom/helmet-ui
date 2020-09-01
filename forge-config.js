846103cdc1feb46bfde4d8ae890dc49faf91cfbd
West Jens <Jens.West@hsl.fi>
2020-09-01 15:44:23 +0300

    Change icon url

--- forge-config.js
+++ forge-config.js
@@ -16,7 +16,7 @@ module.exports = {
             name: "@electron-forge/maker-squirrel",
             config: {
                 name: "helmet-ui",
-                iconUrl: "helmet.ico",
+                iconUrl: "https://raw.githubusercontent.com/HSLdevcom/helmet-ui/e3714d944c62c6ba4b8ed1ce8ac57fcbecaf0f13/helmet.ico",
                 // loadingGif: '',
                 // setupIcon: '',
             }
