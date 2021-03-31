dfeb2efc5886213ed017543ad4c3de43b43e1b4c
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-03-31 16:56:29 +0300

    Pass scenario name and results data folder path
    Running HELMET without EMME failed before. The reason was that
    results folder paths were not passed to helmet_validate_inputs.py.
    Instead, they were read from dev-config.json.
    
    Closes #89

--- src/background/helmet_entrypoint_python_shell.js
+++ src/background/helmet_entrypoint_python_shell.js
@@ -22,6 +22,8 @@ module.exports = {
           "--log-level", allRunParameters[0].log_level,
           "--log-format", "JSON",
           "--baseline-data-path", allRunParameters[0].base_data_folder_path,
+          "--results-path", allRunParameters[0].results_data_folder_path,
+          "--scenario-name", allRunParameters[0].name,
         ]
           .concat(["--emme-paths"]).concat(allRunParameters.map(p => p.emme_project_file_path))
           .concat(["--first-scenario-ids"]).concat(allRunParameters.map(p => p.first_scenario_id))
