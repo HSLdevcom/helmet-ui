7f7f2919f5f5a0072a17e84c388cc55f36168a73
Johanna Piipponen <johanna.piipponen@hsl.fi>
2021-11-01 17:12:30 +0200

    Find Python 3 under Emme 4.5.0

--- src/renderer/components/Settings/Settings.jsx
+++ src/renderer/components/Settings/Settings.jsx
@@ -22,7 +22,7 @@ const Settings = ({
         </button>
         <div className="Settings__dialog-heading">Projektin asetukset</div>
         <div className="Settings__dialog-input-group">
-          <span className="Settings__pseudo-label">EMME Python (v2.7)</span>
+          <span className="Settings__pseudo-label">Emme Python (v3.7)</span>
           <label className="Settings__pseudo-file-select" htmlFor="hidden-input-emme-python-path" title={emmePythonPath}>
             {emmePythonPath ? path.basename(emmePythonPath) : "Valitse.."}
           </label>
