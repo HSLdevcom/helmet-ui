1fb5b9cf4e45f64fcb9d455c314b93fc3a7220fd
Erkki Halinen <erkki-eemeli.halinen@cgi.com>
2023-06-14 13:23:24 +0300

    Tweak chart configs, add axis titles

--- src/renderer/components/HelmetProject/Runtime/RunStatus/RunStatus.jsx
+++ src/renderer/components/HelmetProject/Runtime/RunStatus/RunStatus.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
-import { Chart as ChartJS, LinearScale, LineElement, PointElement, CategoryScale, Tooltip } from "chart.js";
+import { Chart as ChartJS, LinearScale, LineElement, PointElement, CategoryScale, Tooltip, Legend, Title } from "chart.js";
 import { Line } from 'react-chartjs-2';
 import dayjs from 'dayjs';
 var duration = require('dayjs/plugin/duration');
 var relativeTime = require('dayjs/plugin/relativeTime');
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
-ChartJS.register(LinearScale, LineElement, CategoryScale, PointElement, Tooltip);
+ChartJS.register(LinearScale, LineElement, CategoryScale, PointElement, Tooltip, Legend, Title);
 
 
 const RunStatus = ({isScenarioRunning, statusIterationsTotal, statusIterationsCompleted, statusReadyScenariosLogfiles, statusRunStartTime, statusRunFinishTime, statusState, demandConvergenceArray}) => {
+
+  const graphConfig = {
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Convergence',
+          align: 'Center'
+        },
+        legend: {
+          display: false
+        }
+      },
+      scales: {
+        y: { 
+          title: {
+            display: true,
+            text: 'Rel_Gap [ % ]',
+            font: {
+              size: 16
+            }
+          }
+         },
+        x: {
+          title: {
+            display: true,
+            text: 'Iteration [ # ]',
+            font: {
+              size: 16
+            }
+          }
+        }
+      }
+    },
+  }
 
   const graphData = {
     labels: demandConvergenceArray.map(listing => listing.iteration),
@@ -20,13 +55,6 @@ const RunStatus = ({isScenarioRunning, statusIterationsTotal, statusIterationsCo
       borderColor: '#007AC9'
     }
     ],
-    options: {
-      plugins: {
-        legend: {
-          display: true
-        }
-      }
-    }
   }
 
   return (
@@ -35,7 +63,7 @@ const RunStatus = ({isScenarioRunning, statusIterationsTotal, statusIterationsCo
         (statusState === SCENARIO_STATUS_STATE.RUNNING || statusState === SCENARIO_STATUS_STATE.FINISHED) &&
           <div>
             <div className="Status__readiness">
-              <Line className="runtime-chart" data={graphData} />
+              <Line className="runtime-chart" options={graphConfig.options} data={graphData} />
               &nbsp;
             </div>
           </div>
