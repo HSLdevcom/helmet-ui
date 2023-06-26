ca34b0374bc9a56aa7580099227192795bdbc45b
Erkki Halinen <erkki-eemeli.halinen@cgi.com>
2023-06-26 17:06:01 +0300

    #173: Show settings in tooltip when hovering over scenario

--- src/renderer/components/HelmetProject/Runtime/Runtime.jsx
+++ src/renderer/components/HelmetProject/Runtime/Runtime.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Tooltip } from 'react-tooltip'
+import { renderToStaticMarkup } from 'react-dom/server';
 
 const Runtime = ({
   projectPath, scenarios, scenarioIDsToRun, runningScenarioID, openScenarioID, deleteScenario,
@@ -31,8 +33,41 @@ const Runtime = ({
       <div className="Runtime__scenarios">
         {/* Create table of all scenarios "<Button-To-Add-As-Runnable> <Button-To-Open-Configuration>" */}
         {scenarios.map((s) => {
+          // Component for the tooltip showing scenario settings
+          const tooltipContent = (scenario) => {
+            return (
+              <div>
+                {
+                  Object.entries(scenario).map((property) => {
+                    
+                    if(property[0] === 'overriddenProjectSettings') {
+                      return (
+                        <div>
+                          <h3>Overridden settings:</h3>
+                          { 
+                            Object.entries(property[1]).map(overrideSetting => {
+                              return (
+                              <p style={{ marginLeft: "1rem" }}>{overrideSetting.toString().replace(',', ' : ')}</p>
+                            )})
+                          }
+                        </div>
+                      )
+                    }
+
+                    return(
+                      <p>{property.toString().replace(',', ' : ')}</p>
+                    )})}
+              </div>
+            )
+          }
+
           return (
-            <div className="Runtime__scenario" key={s.id}>
+            <div className="Runtime__scenario" key={s.id} 
+              data-tooltip-id="scenario-tooltip" 
+              data-tooltip-place="right" 
+              data-tooltip-html={renderToStaticMarkup(tooltipContent(s))}
+              data-tooltip-delay-show={1000}
+            >
               <span className="Runtime__scenario-name">
                 {s.name ? s.name : `Unnamed project (${s.id})`}
               </span>
@@ -58,6 +93,7 @@ const Runtime = ({
               <div className={"Runtime__scenario-delete"}
                       onClick={(e) => runningScenarioID ? undefined : deleteScenario(s)}
               ></div>
+              <Tooltip id="scenario-tooltip" style={{ borderRadius: "1rem" }}/>
             </div>
           )
         })}
